fd2df5254e50fff276c92a913eb191367d3146b1
admin-xtinex <info@xtinex.com>
2026-08-26 14:46:08 +0530

    Export manage_open_positions

--- src/xora_chart/engines/trade/__init__.py
+++ src/xora_chart/engines/trade/__init__.py
@@ -1,3 +1,8 @@
-from xora_chart.engines.trade.engine import close_position, list_positions, open_position
+from xora_chart.engines.trade.engine import (
+    close_position,
+    list_positions,
+    manage_open_positions,
+    open_position,
+)
 
-__all__ = ["open_position", "close_position", "list_positions"]
+__all__ = ["open_position", "close_position", "list_positions", "manage_open_positions"]
